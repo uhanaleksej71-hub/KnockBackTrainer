5ec8e33fc46dc8c08c4d9076ec048aba5dde3601
uhanaleksej71-hub <uhanaleksej71@gmail.com>
2026-02-09 04:05:23 +0200

    Create ModMenuIntegration.java

---
+++ src/main/java/dev/knockbacktrainer/config/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package dev.knockbacktrainer.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (screen) -> new KnockbackConfigScreen();
+    }
+}
